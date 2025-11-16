d18f731146937d4fb03fe7a4fd71808b5e5b0f38
sarveshmathanraj02 <sarveshmathanraj2@gmail.com>
2025-11-17 01:45:57 +0530

    Update courses.py

--- courses.py
+++ courses.py
@@ -1,4 +1,4 @@
-ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'],
+ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'], 
             ['Machine Learning A-Z by Udemy','https://www.udemy.com/course/machinelearning/'],
             ['Machine Learning by Andrew NG','https://www.coursera.org/learn/machine-learning'],
             ['Data Scientist Master Program of Simplilearn (IBM)','https://www.simplilearn.com/big-data-and-analytics/senior-data-scientist-masters-program-training'],
@@ -8,7 +8,6 @@ ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://develop
             ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
             ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017'],
             ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230']]
-
 
 web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
              ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
@@ -21,7 +20,6 @@ web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
              ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
              ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019']]
 
-
 android_course = [['Android Development for Beginners [Free]','https://youtu.be/fis26HvvDII'],
                  ['Android App Development Specialization','https://www.coursera.org/specializations/android-app-development'],
                  ['Associate Android Developer Certification','https://grow.google/androiddev/#?modal_active=none'],
@@ -33,7 +31,6 @@ android_course = [['Android Development for Beginners [Free]','https://youtu.be/
                  ['Flutter & Dart - The Complete Flutter App Development Course','https://www.udemy.com/course/flutter-dart-the-complete-flutter-app-development-course/'],
                  ['Flutter App Development Course [Free]','https://youtu.be/rZLR5olMR64']]
 
-
 ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learning/subscription/topics/ios'],
              ['iOS & Swift - The Complete iOS App Development Bootcamp','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
              ['Become an iOS Developer','https://www.udacity.com/course/ios-developer-nanodegree--nd003'],
@@ -44,6 +41,7 @@ ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learn
              ['Learn Swift by Codecademy','https://www.codecademy.com/learn/learn-swift'],
              ['Swift Tutorial - Full Course for Beginners [Free]','https://youtu.be/comQ1-x2a1Q'],
              ['Learn Swift Fast - [Free]','https://youtu.be/FcsY1YPBwzQ']]
+
 uiux_course = [['Google UX Design Professional Certificate','https://www.coursera.org/professional-certificates/google-ux-design'],
               ['UI / UX Design Specialization','https://www.coursera.org/specializations/ui-ux-design'],
               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
@@ -55,15 +53,14 @@ uiux_course = [['Google UX Design Professional Certificate','https://www.courser
               ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
               ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']]
 
-
 software_engineering_courses = {
-   # Programming Languages
    "Python": [
        ["Python for Everybody by Coursera [Free]", "https://www.coursera.org/specializations/python"],
        ["Complete Python Bootcamp: Go from Zero to Hero by Udemy",
         "https://www.udemy.com/course/complete-python-bootcamp/"],
        ["Python Data Structures by Coursera", "https://www.coursera.org/learn/python-data"],
    ],
+
    "Java": [
        ["Java Programming and Software Engineering Fundamentals by Coursera",
         "https://www.coursera.org/specializations/java-programming"],
@@ -71,6 +68,7 @@ software_engineering_courses = {
         "https://www.udemy.com/course/java-the-complete-java-developer-course/"],
        ["Learn Java Programming by Codecademy", "https://www.codecademy.com/learn/learn-java"],
    ],
+
    "JavaScript": [
        ["JavaScript: Understanding the Weird Parts by Udemy", "https://www.udemy.com/course/understand-javascript/"],
        ["JavaScript Algorithms and Data Structures by freeCodeCamp [Free]",
@@ -79,13 +77,12 @@ software_engineering_courses = {
         "https://www.udemy.com/course/the-complete-javascript-course/"],
    ],
 
-
-   # Development Tools and Systems
    "Git": [
        ["Git Essential Training by LinkedIn", "https://www.linkedin.com/learning/git-essential-training/"],
        ["Version Control with Git by Coursera", "https://www.coursera.org/learn/version-control-with-git"],
        ["Git & GitHub Crash Course [Free]", "https://www.udemy.com/course/git-and-github-crash-course/"],
    ],
+
    "Docker": [
        ["Docker for Beginners by LinkedIn", "https://www.linkedin.com/learning/docker-for-beginners/"],
        ["Docker and Kubernetes: The Complete Guide by Udemy",
@@ -93,8 +90,6 @@ software_engineering_courses = {
        ["Docker Essentials by Pluralsight", "https://www.pluralsight.com/courses/docker-essentials"],
    ],
 
-
-   # Web Development
    "HTML & CSS": [
        ["HTML and CSS for Beginners by Coursera [Free]",
         "https://www.coursera.org/projects/html-and-css-for-beginners"],
@@ -103,33 +98,32 @@ software_engineering_courses = {
        ["Responsive Web Design Certification by freeCodeCamp [Free]",
         "https://www.freecodecamp.org/learn/responsive-web-design/"],
    ],
+
    "React": [
        ["React - The Complete Guide by Udemy", "https://www.udemy.com/course/react-the-complete-guide-incl-redux/"],
        ["React Basics by Codecademy", "https://www.codecademy.com/learn/react-101"],
        ["Modern React with Redux by Udemy", "https://www.udemy.com/course/react-redux/"],
    ],
 
-
-   # Databases
    "SQL": [
        ["SQL for Data Science by Coursera [Free]", "https://www.coursera.org/learn/sql-for-data-science"],
        ["The Complete SQL Bootcamp by Udemy", "https://www.udemy.com/course/the-complete-sql-bootcamp/"],
        ["Intro to SQL by DataCamp", "https://www.datacamp.com/courses/intro-to-sql"],
    ],
+
    "MongoDB": [
        ["MongoDB Essentials by MongoDB University [Free]", "https://university.mongodb.com/"],
        ["Complete MongoDB Guide by Udemy", "https://www.udemy.com/course/mongodb-the-complete-developers-guide/"],
        ["Introduction to MongoDB by Codecademy", "https://www.codecademy.com/learn/learn-mongodb"],
    ],
 
-
-   # Testing and Debugging
    "Unit Testing": [
        ["Unit Testing for Software Engineers by Coursera", "https://www.coursera.org/learn/unit-testing"],
        ["JUnit and Mockito Crash Course [Free]", "https://www.udemy.com/course/junit-and-mockito-crash-course/"],
        ["Automated Software Testing by LinkedIn",
         "https://www.linkedin.com/learning/automated-software-testing/introduction"],
    ],
+
    "Selenium": [
        ["Selenium WebDriver with Java by Udemy", "https://www.udemy.com/course/selenium-webdriver-tutorials/"],
        ["Learn Selenium from Scratch by LinkedIn", "https://www.linkedin.com/learning/learning-selenium/"],
@@ -137,8 +131,6 @@ software_engineering_courses = {
         "https://www.coursera.org/learn/test-automation-python-selenium"],
    ],
 
-
-   # DevOps and Security
    "CI/CD": [
        ["Continuous Integration and Continuous Deployment by Coursera",
         "https://www.coursera.org/learn/continuous-integration"],
@@ -146,6 +138,7 @@ software_engineering_courses = {
        ["Jenkins: Continuous Integration for Developers by Pluralsight",
         "https://www.pluralsight.com/courses/jenkins-continuous-integration"],
    ],
+
    "Cybersecurity": [
        ["Introduction to Cyber Security by Coursera", "https://www.coursera.org/learn/intro-cyber-security"],
        ["Cybersecurity Specialization by Coursera", "https://www.coursera.org/specializations/cyber-security"],
@@ -153,8 +146,6 @@ software_engineering_courses = {
         "https://www.udemy.com/course/become-a-cyber-security-specialist/"],
    ],
 
-
-   # General Skills
    "Project Management": [
        ["Foundations of Project Management by Google [Free]",
         "https://www.coursera.org/learn/project-management-foundations"],
@@ -162,6 +153,7 @@ software_engineering_courses = {
         "https://www.linkedin.com/learning/project-management-foundations/"],
        ["Agile Project Management by Coursera", "https://www.coursera.org/learn/agile-project-management"],
    ],
+
    "Communication": [
        ["Communication Foundations by LinkedIn", "https://www.linkedin.com/learning/communication-foundations"],
        ["Effective Communication by Coursera", "https://www.coursera.org/learn/effective-communication"],
@@ -169,17 +161,12 @@ software_engineering_courses = {
    ],
 }
 
-
 resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
                 'https://youtu.be/yp693O87GmM','https://youtu.be/UeMmCex9uTU',
                 'https://youtu.be/dQ7Q8ZdnuN0','https://youtu.be/HQqqQx5BCFY',
                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
 
-
 interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
                    'https://youtu.be/9FgfsLa_SmY','https://youtu.be/2HQmjLu-6RQ',
-                   'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w'
+                   'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w',
                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
-
-
-
